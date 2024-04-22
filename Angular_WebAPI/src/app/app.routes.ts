dff4dbdb750fe2e61851c35a9d9d41dd917c6acc
Markus <markus@haslinger.it>
2024-04-22 16:19:27 +0200

    added auth guard and role determination

--- Angular_WebAPI/src/app/app.routes.ts
+++ Angular_WebAPI/src/app/app.routes.ts
@@ -2,10 +2,19 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {CounterComponent} from "./counter/counter.component";
 import {ApiDemoComponent} from "./api-demo/api-demo.component";
+import { AuthGuard } from "../core/util/auth-guard";
+import { Role } from "../core/util/leo-token";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'counter', component: CounterComponent},
+  {
+    path: 'counter',
+    component: CounterComponent,
+    canActivate: [AuthGuard],
+    data: {
+      roles: [Role.Student]
+    }
+  },
   {path: 'api-demo', component: ApiDemoComponent}
 ];
