d2b8accbef7c44216a32efb5a9d1e4432cea5e0d
Markus <markus@haslinger.it>
2024-04-22 17:14:35 +0200

    added api call example with bearer token

--- Angular_WebAPI/src/app/api-demo/api-demo.component.ts
+++ Angular_WebAPI/src/app/api-demo/api-demo.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, Signal, signal, WritableSignal } from "@angular/core";
+import { MatButton } from "@angular/material/button";
+import { HttpClient } from "@angular/common/http";
+import { finalize } from "rxjs";
 
 @Component({
   selector: 'app-api-demo',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './api-demo.component.html',
   styleUrl: './api-demo.component.scss'
 })
 export class ApiDemoComponent {
+  private readonly httpClient: HttpClient = inject(HttpClient);
+  public readonly response: WritableSignal<string | null> = signal(null);
+  public readonly loading: WritableSignal<boolean> = signal(false);
+  public readonly showResponse: Signal<boolean> = computed(() => this.response() !== null);
 
+  public performCall(action: string): void {
+    const route = `http://localhost:5050/api/demo/${action}`;
+
+    this.loading.set(true);
+
+    // bearer token is automatically added by the interceptor
+    this.httpClient.get(route, { responseType: "text" })
+      .pipe(finalize(() => this.loading.set(false)))
+      .subscribe({
+        next: (res) => {
+          this.response.update(() => res);
+        },
+        error: err => {
+          this.response.update(() => `Backend says no: ${err.status}`);
+        }
+      });
+  }
 }
